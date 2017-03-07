003aaae06f44745433bef4b589e11a804634ca58
bramspeelman <bramspeelman@gmail.com>
2017-03-07 15:35:26 +0100

    Print out the Ingredients

--- spagetti.rb
+++ spagetti.rb
@@ -1,3 +1,14 @@
+def print_divider
+  puts "*" * 40
+  puts "\n"
+end
+
+def print_progress_bar
+  3.times { sleep 0.5; print "." }
+  puts "\n"
+end
+
+
 NUM_EGGS = 5
 NUM_CHEESE =6
 
@@ -6,3 +17,12 @@ ingredients = [
   {name: "cheese", quantity: NUM_CHEESE},
   {name:"onion", quantity:1},
   {name:"oil", quantity:"some"}]
+
+puts "* Let's cook an omelette with cheese! *"
+print_divider
+
+puts "You need the following ingredients:"
+
+ingredients.each do |ingredient|
+  puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
+end
